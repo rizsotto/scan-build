742b19f84287083e3fedc6d8adeaa703f70126cb
Laszlo Nagy <rizsotto@gmail.com>
2013-10-01 16:34:06 +0200

    add necessary python package files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#from setuptools import setup
+from distutils.core import setup
+
+setup(
+    name='beye',
+    version='0.1',
+    author='László Nagy',
+    author_email='rizsotto@gmail.com',
+    keywords=['clang', 'scan-build', 'checker', 'analyzer', 'static analyzer'],
+    scripts=['bin/beye'],
+    url='https://github.com/rizsotto/Beye',
+    license='LICENSE.txt',
+    description='static code analyzer wrapper for Clang.',
+    long_description=open('README').read()
+)
