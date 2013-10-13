1bbfcaa2cbee75d50a21800e81863793a60623c8
Laszlo Nagy <rizsotto@gmail.com>
2013-10-13 23:42:01 +0200

    add compiler action detection method

--- beye/analyze.py
+++ beye/analyze.py
@@ -8,95 +8,121 @@ import logging
 
 
 _CompileOptionMap = {
-    '-nostdinc' : 0,
-    '-fblocks' : 0,
-    '-fno-builtin' : 0,
-    '-fobjc-gc-only' : 0,
-    '-fobjc-gc' : 0,
-    '-ffreestanding' : 0,
-    '-include' : 1,
-    '-idirafter' : 1,
-    '-imacros' : 1,
-    '-iprefix' : 1,
-    '-iquote' : 1,
-    '-isystem' : 1,
-    '-iwithprefix' : 1,
-    '-iwithprefixbefore' : 1
+    '-nostdinc': 0,
+    '-include': 1,
+    '-idirafter': 1,
+    '-imacros': 1,
+    '-iprefix': 1,
+    '-iquote': 1,
+    '-isystem': 1,
+    '-iwithprefix': 1,
+    '-iwithprefixbefore': 1
 }
 
 _LinkerOptionMap = {
-    '-framework' : 1,
-    '-fobjc-link-runtime' : 0
+    '-framework': 1,
+    '-fobjc-link-runtime': 0
 }
 
 _CompilerLinkerOptionMap = {
-    '-fobjc-arc' : 0,
-    '-fno-objc-arc' : 0,
-    '-fobjc-abi-version' : 0, # This is really a 1 argument, but always has '='
-    '-fobjc-legacy-dispatch' : 0,
-    '-mios-simulator-version-min' : 0, # This really has 1 argument, but always has '='
-    '-isysroot' : 1,
-    '-arch' : 1,
-    '-m32' : 0,
-    '-m64' : 0,
-    '-stdlib' : 0, # This is really a 1 argument, but always has '='
-    '-v' : 0,
-    '-fpascal-strings' : 0,
-    '-mmacosx-version-min' : 0, # This is really a 1 argument, but always has '='
-    '-miphoneos-version-min' : 0 # This is really a 1 argument, but always has '='
+    '-Wwrite-strings': 0,
+    # specifically call out separated -f flag
+    '-ftrapv-handler': 1,
+    # This is really a 1 argument, but always has '='
+    '-mios-simulator-version-min': 0,
+    '-isysroot': 1,
+    '-arch': 1,
+    '-m32': 0,
+    '-m64': 0,
+    # This is really a 1 argument, but always has '='
+    '-stdlib': 0,
+    '-target': 1,
+    '-v': 0,
+    # This is really a 1 argument, but always has '='
+    '-mmacosx-version-min': 0,
+    # This is really a 1 argument, but always has '='
+    '-miphoneos-version-min': 0
 }
 
 _IgnoredOptionMap = {
-    '-MT' : 1,  # Ignore these preprocessor options.
-    '-MF' : 1,
+    '-MT': 1,  # Ignore these preprocessor options.
+    '-MF': 1,
 
-    '-fsyntax-only' : 0,
-    '-save-temps' : 0,
-    '-install_name' : 1,
-    '-exported_symbols_list' : 1,
-    '-current_version' : 1,
-    '-compatibility_version' : 1,
-    '-init' : 1,
-    '-e' : 1,
-    '-seg1addr' : 1,
-    '-bundle_loader' : 1,
-    '-multiply_defined' : 1,
-    '-sectorder' : 3,
-    '--param' : 1,
-    '-u' : 1,
-    '--serialize-diagnostics' : 1
+    '-fsyntax-only': 0,
+    '-save-temps': 0,
+    '-install_name': 1,
+    '-exported_symbols_list': 1,
+    '-current_version': 1,
+    '-compatibility_version': 1,
+    '-init': 1,
+    '-e': 1,
+    '-seg1addr': 1,
+    '-bundle_loader': 1,
+    '-multiply_defined': 1,
+    '-sectorder': 3,
+    '--param': 1,
+    '-u': 1,
+    '--serialize-diagnostics': 1
 }
 
 _LangMap = {
-    'c'   : 'c',
-    'cp'  : 'c++',
-    'cpp' : 'c++',
-    'cxx' : 'c++',
-    'txx' : 'c++',
-    'cc'  : 'c++',
-    'C'   : 'c++',
-    'ii'  : 'c++',
-    'i'   : 'c-cpp-output',
-    'm'   : 'objective-c',
-    'mi'  : 'objective-c-cpp-output',
-    'mm'  : 'objective-c++'
+    'c': 'c',
+    'cp': 'c++',
+    'cpp': 'c++',
+    'cxx': 'c++',
+    'txx': 'c++',
+    'cc': 'c++',
+    'C': 'c++',
+    'ii': 'c++',
+    'i': 'c-cpp-output',
+    'm': 'objective-c',
+    'mi': 'objective-c-cpp-output',
+    'mm': 'objective-c++'
 }
 
 _UniqueOptions = {
-    '-isysroot' : 0
+    '-isysroot': 0
 }
 
 _LangsAccepted = {
-    "c" : 1,
-    "c++" : 1,
-    "objective-c" : 1,
-    "objective-c++" : 1
+    "c": 1,
+    "c++": 1,
+    "objective-c": 1,
+    "objective-c++": 1
 }
 
 _DisabledArchs = {
-    'ppc' : 1,
-    'ppc64' : 1
+    'ppc': 1,
+    'ppc64': 1
 }
+
+
+class Action:
+    Info, Link, Compile, Preprocess = range(4)
+
+    import re
+    _preprocess_regex = re.compile('^-(E|MM?)$')
+    _info_regex = re.compile('^-print-prog-name')
+
+    def is_preprocess(arg):
+        return True if (Action._preprocess_regex.match(arg)) else False
+
+    def is_compile(arg):
+        return True if ('-c' == arg) else False
+
+    def is_info(arg):
+        return True if (Action._info_regex.match(arg)) else False
+
+    def what(args):
+        result = Action.Link
+        for arg in args:
+            if (Action.is_info(arg)):
+                return Action.Info
+            elif (Action.is_preprocess(arg)):
+                return Action.Preprocess
+            elif (Action.is_compile(arg)):
+                result = Action.Compile
+        return result
 
 
 def split_arguments(args):
@@ -117,4 +143,3 @@ class Analyzer:
         compilation = subprocess.Popen(cmd, env=os.environ)
         compilation.wait()
         return compilation.returncode
-
--- test/test_analyze.py
+++ test/test_analyze.py
@@ -9,3 +9,24 @@ def test_split_arguments():
     input = ['clang', '-Wall', '-std=C99', '-v', 'file.c']
     expected = ['clang', '-Wall', '-std', 'C99', '-v', 'file.c']
     assert expected == sut.split_arguments(input)
+
+
+def test_action():
+    def test(expected, cmds):
+        assert expected == sut.Action.what(cmds)
+
+    Info = sut.Action.Info
+    test(Info, ['clang', 'source.c', '-print-prog-name'])
+
+    Link = sut.Action.Link
+    test(Link, ['clang', 'source.c'])
+
+    Compile = sut.Action.Compile
+    test(Compile, ['clang', '-c', 'source.c'])
+    test(Compile, ['clang', '-c', 'source.c', '-MF', 'source.d'])
+
+    Preprocess = sut.Action.Preprocess
+    test(Preprocess, ['clang', '-E', 'source.c'])
+    test(Preprocess, ['clang', '-c', '-E', 'source.c'])
+    test(Preprocess, ['clang', '-c', '-M', 'source.c'])
+    test(Preprocess, ['clang', '-c', '-MM', 'source.c'])
