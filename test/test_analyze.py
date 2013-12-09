6e3faf2f84874e5f040837c3363a7c6b2b5c066f
Laszlo Nagy <rizsotto@gmail.com>
2013-12-10 00:28:02 +0100

    review test cases

--- test/test_analyze.py
+++ test/test_analyze.py
@@ -4,31 +4,56 @@
 # This file is part of Beye [see file LICENSE.txt for more]
 
 import beye.analyze as sut
+from nose.tools import assert_equals
 
 
 def test_language_from_filename():
-    assert 'c' == sut.language_from_filename('file.c')
-    assert 'c' == sut.language_from_filename('./file.c')
-    assert 'c' == sut.language_from_filename('./path/file.c')
-    assert 'c' == sut.language_from_filename('./path.cpp/file.c')
-    assert 'c' == sut.language_from_filename('/abs/path/file.c')
-    assert 'c++' == sut.language_from_filename('./file.cpp')
-    assert 'c++' == sut.language_from_filename('./file.cxx')
+    assert_equals('c', sut.language_from_filename('file.c'))
+    assert_equals('c', sut.language_from_filename('./file.c'))
+    assert_equals('c', sut.language_from_filename('./path/file.c'))
+    assert_equals('c', sut.language_from_filename('./path.cpp/file.c'))
+    assert_equals('c', sut.language_from_filename('/abs/path/file.c'))
+    assert_equals('c++', sut.language_from_filename('./file.cpp'))
+    assert_equals('c++', sut.language_from_filename('./file.cxx'))
 
 
-def test_is_accepted_language():
-    assert True == sut.is_accepted_language('c')
-    assert True == sut.is_accepted_language('c++')
-    assert True == sut.is_accepted_language('objective-c')
-    assert True == sut.is_accepted_language('objective-c++')
-    assert False == sut.is_accepted_language('abap')
-    assert False == sut.is_accepted_language(None)
+def test_set_language():
+    def test(expected, input):
+        class Spy:
+            def __init__(self):
+                self.result = None
+            def catch(self, out):
+                self.result = out
+
+        spy = Spy()
+        sut.set_language(input, spy.catch)
+        assert_equals(expected, spy.result)
+
+    l = 'language'
+    f = 'file'
+    test({f: 'file.c', l: 'c'}, {f: 'file.c', l: 'c'})
+    test({f: 'file.c', l: 'c++'}, {f: 'file.c', l: 'c++'})
+    test({f: 'file.c', l: 'c'}, {f: 'file.c'})
+    test({f: 'file.cxx', l: 'c++'}, {f: 'file.cxx'})
+    test(None, {f: 'file.i'})
+    test(None, {f: 'file.i', l: 'c-cpp-output'})
+    test(None, {f: 'file.java'})
 
 
-def test_action():
+def test_archs_seen():
+    def test(cmd):
+        opts = sut.parse(cmd.split())
+        return opts.get('archs_seen', set())
+
+    assert_equals(set(), test('clang -c source.c'))
+    assert_equals(set(['ppc']), test('clang -c -arch ppc source.c'))
+    assert_equals(set(['ppc', 'i386']), test('clang -c -arch ppc -arch i386 source.c'))
+
+
+def test_parse_action():
     def test(expected, cmd):
         opts = sut.parse(cmd.split())
-        assert expected == opts['action']
+        assert_equals(expected, opts['action'])
 
     Info = sut.Action.Info
     test(Info, 'clang source.c -print-prog-name')
@@ -47,51 +72,63 @@ def test_action():
     test(Preprocess, 'clang -c -MM source.c')
 
 
-def test_archs_seen():
-    def test(cmd):
-        opts = sut.parse(cmd.split())
-        return opts.get('archs_seen', set())
-
-    assert set() == test('clang -c source.c')
-    assert set(['ppc']) == test('clang -c -arch ppc source.c')
-    assert set(['ppc', 'i386']) == test('clang -c -arch ppc -arch i386 source.c')
-
-
-def test_compile_flags():
+def test_parse_compile_opts():
     def test(cmd):
         opts = sut.parse(cmd.split())
         return opts.get('compile_options', [])
 
-    assert [] == test('clang source.c')
-    assert ['-nostdinc', '-include', '/tmp'] == \
-        test('clang -c -nostdinc source.c -include /tmp')
+    assert_equals([], test('clang source.c'))
+    assert_equals([], test('clang source.c -g -Wall'))
+    assert_equals(['-nostdinc'], test('clang -c -nostdinc source.c'))
+    assert_equals(['-fPIC'], test('clang -c source.c -fPIC'))
 
 
-def test_complex_1():
-    cmd = 'clang -c -Wall -g -o source.o source.c -std=C99 -fpic ' \
-          '-arch i386 -O3 -x c'
+def test_parse_optimalizations():
+    def test(cmd):
+        opts = sut.parse(cmd.split())
+        return opts.get('compile_options', [])
+
+    assert_equals(['-O1'], test('clang -c source.c -O'))
+    assert_equals(['-O1'], test('clang -c source.c -O1'))
+    assert_equals(['-O2'], test('clang -c source.c -Os'))
+    assert_equals(['-O2'], test('clang -c source.c -O2'))
+    assert_equals(['-O3'], test('clang -c source.c -O3'))
+
+
+def test_parse_input_file():
+    cmd = 'clang -c -o source.o source.c -Wall -g'
     opts = sut.parse(cmd.split())
-    assert opts['files'] == ['source.c']
-    assert opts['output'] == 'source.o'
-    assert opts['language'] == 'c'
-    assert opts['action'] == sut.Action.Compile
-    assert opts['archs_seen'] == set(['i386'])
-    assert opts['compile_options'] == ['-std=C99', '-fpic', '-arch', 'i386', '-O3']
+    assert_equals(opts['files'], ['source.c'])
+    assert_equals(opts['output'], 'source.o')
+    assert_equals(opts.get('language'), None)
+    assert_equals(opts.get('archs_seen'), None)
 
 
-def test_complex_2():
-    cmd = 'clang -c -o source.o source.c -I/usr/local/include'
+def test_parse_language():
+    cmd = 'clang -c -o source.o source.c -x cpp'
     opts = sut.parse(cmd.split())
-    assert opts['files'] == ['source.c']
-    assert opts['output'] == 'source.o'
-    assert opts.get('language') == None
-    assert opts['action'] == sut.Action.Compile
-    assert opts.get('archs_seen') == None
-    assert opts['compile_options'] == ['-I/usr/local/include']
+    assert_equals(opts['language'], 'cpp')
 
 
-def test_new():
+def test_parse_arch():
+    cmd = 'clang -c -o source.o source.c -arch i386 -arch mips'
+    opts = sut.parse(cmd.split())
+    assert_equals(opts.get('archs_seen'), set(['i386', 'mips']))
+
+
+def test_parse_include():
     cmd = 'clang -c -o source.o source.c -include /usr/local/include'
     opts = sut.parse(cmd.split())
-    assert opts['action'] == sut.Action.Compile
-    assert opts['compile_options'] == ['-include', '/usr/local/include']
+    assert_equals(opts['compile_options'], ['-include', '/usr/local/include'])
+
+
+def test_parse_includes():
+    cmd = 'clang -c -o source.o source.c -I/usr/local/include -I .'
+    opts = sut.parse(cmd.split())
+    assert_equals(opts['compile_options'], ['-I/usr/local/include', '-I', '.'])
+
+
+def test_parse_defines():
+    cmd = 'clang -c -o source.o source.c -DNDEBUG -Dvariable=value'
+    opts = sut.parse(cmd.split())
+    assert_equals(opts['compile_options'], ['-DNDEBUG', '-Dvariable=value'])
