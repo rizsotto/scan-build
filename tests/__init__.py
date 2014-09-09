44a0c9e230b05d796a9ea015336d563df9f65b82
Laszlo Nagy <rizsotto@gmail.com>
2014-09-09 12:34:09 +0200

    add copyright header for test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+#                     The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+
 import unittest
 
 import tests.unit
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+#                     The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+
 from . import test_parse
 from . import test_analyze
 from . import test_get_clang_arguments
