dc121994be19f92b3d811c40ed219146827c8eb1
Laszlo Nagy <rizsotto@gmail.com>
2015-05-03 21:49:29 +0200

    test: exec anatomy is back in tests

--- tests/functional/cases/test_exec_anatomy.py
+++ tests/functional/cases/test_exec_anatomy.py
@@ -12,19 +12,23 @@ import subprocess
 import json
 
 
-def run(target_dir):
-    this_dir, _ = os.path.split(__file__)
-    test_executable = os.path.join(target_dir, 'exec_anatomy')
+def run(source_dir, target_dir):
+    def execute(cmd):
+        return subprocess.check_call(cmd, cwd=target_dir,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT)
+
+    execute(['cmake', source_dir])
+    execute(['make'])
+
     result_file = os.path.join(target_dir, 'result.json')
     expected_file = os.path.join(target_dir, 'expected.json')
-    subprocess.check_call(
-        ['scan-build', 'intercept', '--cdb', result_file,
-         test_executable, expected_file],
-        cwd=target_dir)
+    execute(['scan-build', 'intercept', '--cdb', result_file,
+             './exec', expected_file])
     return (expected_file, result_file)
 
 
-class OutputDirectoryTest(unittest.TestCase):
+class ExecAnatomyTest(unittest.TestCase):
 
     def assertEqualJson(self, expected, result):
         def read_json(filename):
@@ -38,8 +42,9 @@ class OutputDirectoryTest(unittest.TestCase):
         for item in rhs:
             self.assertTrue(lhs.count(item))
 
-    @unittest.skip("running exec_anatomy from build dir shall be implemented")
-    def test_regular_keeps_report_dir(self):
-        with fixtures.TempDir() as tmpdir:
-            expected, result = run(tmpdir)
+    def test_all_exec_calls(self):
+        this_dir, _ = os.path.split(__file__)
+        source_dir = os.path.normpath(os.path.join(this_dir, '..', 'exec'))
+        with fixtures.TempDir() as tmp_dir:
+            expected, result = run(source_dir, tmp_dir)
             self.assertEqualJson(expected, result)
