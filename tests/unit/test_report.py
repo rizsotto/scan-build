754713b364c10f1f6b64458f535235e56c313a29
Laszlo Nagy <rizsotto@gmail.com>
2016-09-07 20:20:25 +1000

    D24163: Increase precision of timestamp in report directory name

--- tests/unit/test_report.py
+++ tests/unit/test_report.py
@@ -146,3 +146,17 @@ class GetPrefixFromCompilationDatabaseTest(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(
             sut.commonprefix([]), '')
+
+
+class ReportDirectoryTest(unittest.TestCase):
+
+    # Test that successive report directory names ascend in lexicographic
+    # order. This is required so that report directories from two runs of
+    # scan-build can be easily matched up to compare results.
+    def test_directory_name_comparison(self):
+        with libear.temporary_directory() as tmp_dir, \
+             sut.report_directory(tmp_dir, False) as report_dir1, \
+             sut.report_directory(tmp_dir, False) as report_dir2, \
+             sut.report_directory(tmp_dir, False) as report_dir3:
+            self.assertLess(report_dir1, report_dir2)
+            self.assertLess(report_dir2, report_dir3)
