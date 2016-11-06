0cdb21c5e0890e7ac7607cf9e9f833d5b1928157
Laszlo Nagy <rizsotto@gmail.com>
2016-11-06 23:19:06 +1100

    add test for shell_split method

---
+++ tests/unit/test_libscanbuild.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#                     The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+#
+# RUN: %{python} %s
+
+import libscanbuild as sut
+import unittest
+
+
+class ShellSplitTest(unittest.TestCase):
+
+    def test_regular_commands(self):
+        self.assertEqual([], sut.shell_split(""))
+        self.assertEqual(['clang', '-c', 'file.c'],
+                         sut.shell_split('clang -c file.c'))
+        self.assertEqual(['clang', '-c', 'file.c'],
+                         sut.shell_split('clang  -c  file.c'))
+        self.assertEqual(['clang', '-c', 'file.c'],
+                         sut.shell_split('clang -c\tfile.c'))
+
+    def test_quoted_commands(self):
+        self.assertEqual(['clang', '-c', 'file.c'],
+                         sut.shell_split('"clang" -c "file.c"'))
+        self.assertEqual(['clang', '-c', 'file.c'],
+                         sut.shell_split("'clang' -c 'file.c'"))
+
+    def test_shell_escaping(self):
+        self.assertEqual(['clang', '-c', 'file.c', '-Dv=space value'],
+                         sut.shell_split('clang -c file.c -Dv="space value"'))
+        self.assertEqual(['clang', '-c', 'file.c', '-Dv=\"quote'],
+                         sut.shell_split('clang -c file.c -Dv=\\\"quote'))
+        self.assertEqual(['clang', '-c', 'file.c', '-Dv=(word)'],
+                         sut.shell_split('clang -c file.c -Dv=\(word\)'))
+
+
+if __name__ == '__main__':
+    unittest.main()
