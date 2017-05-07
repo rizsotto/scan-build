dc73f5d028baffd37c1b1c8d8719843b4723a71d
Laszlo Nagy <rizsotto@gmail.com>
2017-05-07 17:36:53 +1000

    bump up version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='scan-build',
-    version='2.0.3',
+    version='2.0.4',
     author='László Nagy',
     author_email='rizsotto@gmail.com',
     keywords=['Clang', 'scan-build', 'static analyzer'],
